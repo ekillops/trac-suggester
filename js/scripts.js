a9f8851314e17668c5d2072fa9ccd4d2dfc6ea4c
Erik Killops <erik.killops@gmail.com>
2016-10-21 12:56:01 -0700

    create ids and variables for each question to track answers

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+var apps = $('#apps').is(':checked');
+var mobile = $('#mobile').is(':checked');
+var large = $('#large').is(':checked');
+var design = $('#design').is(':checked');
+var curly = $('#curly').is(':checked');
+
+$
+
+
+
+//event.preventDefault();
+});
