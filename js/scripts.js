bb717bfd20e27830287704c37be8c53b4097764d
Erik Killops <erik.killops@gmail.com>
2016-10-21 13:48:46 -0700

    move radio variables into click function, add function to display results based on input

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,37 @@
 
 //Front End
-
+var chooseLang = function(curly, mobile, design, apps) {
+  if (curly === true) {
+    if (mobile === true) {
+      $("#java").show();
+    }
+    else if (design === true) {
+      $("#css").show();
+    } else {
+      $("#c-sharp").show();
+    }
+  } else if (apps === true) {
+    $("#ruby").show();
+  } else {
+    $("#php").show();
+  }
+};
 
 $(document).ready(function() {
-var apps = $('#apps').is(':checked');
-var mobile = $('#mobile').is(':checked');
-var large = $('#large').is(':checked');
-var design = $('#design').is(':checked');
-var curly = $('#curly').is(':checked');
-var javaLang = false;
-var cSharpLang = false;
-var phpLang = false;
-var rubyLang = false;
-var cssLang = false;
 
 
+$("#submit-survey").click(function() {
+  $(".survey-result").hide();
 
-$("#submit-survey").click(function(){
+  /*chooseLang does not receive input if variables are defined outside of click event*/
+  var apps = $('#apps').is(':checked');
+  var mobile = $('#mobile').is(':checked');
+  var large = $('#large').is(':checked');
+  var design = $('#design').is(':checked');
+  var curly = $('#curly').is(':checked');
 
+
+  chooseLang(curly, mobile, design, apps);
 });
 
 //event.preventDefault();
