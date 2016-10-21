4cae3a4575f52511f10315e777565b12f5fb4895
Erik Killops <erik.killops@gmail.com>
2016-10-21 13:05:26 -0700

    create variables for each language

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,24 @@
+
+//Front End
+
+
 $(document).ready(function() {
 var apps = $('#apps').is(':checked');
 var mobile = $('#mobile').is(':checked');
 var large = $('#large').is(':checked');
 var design = $('#design').is(':checked');
 var curly = $('#curly').is(':checked');
+var javaLang = false;
+var cSharpLang = false;
+var phpLang = false;
+var rubyLang = false;
+var cssLang = false;
 
-$
 
 
+$("#submit-survey").click(function(){
+
+});
 
 //event.preventDefault();
 });
