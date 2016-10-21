3b2bc59ded0befc8c9409f6d9949fb948e9b62a2
Erik Killops <erik.killops@gmail.com>
2016-10-21 15:46:03 -0700

    reworked branching function to be more logical with language choice

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,36 @@
 
 //Front End
-var chooseLang = function(curly, mobile, design, apps) {
-  if (curly === true) {
+
+//Very specific function. Not to be reused in general contexts.
+var chooseLang = function(apps, mobile, large, design) {
+  if (apps === true) {
     if (mobile === true) {
       $("#java").show();
-    }
-    else if (design === true) {
-      $("#css").show();
-    } else {
+    } else if (large === true) {
       $("#c-sharp").show();
+    } else {
+      $("#ruby").show();
     }
-  } else if (apps === true) {
-    $("#ruby").show();
-  } else {
+  } else if (design === true) {
+    $("#css").show();
+  }  else {
     $("#php").show();
   }
 };
 
 $(document).ready(function() {
 
+  $("#submit-survey").click(function() {
+    $(".survey-result").hide();
 
-$("#submit-survey").click(function() {
-  $(".survey-result").hide();
+    /*chooseLang does not receive input if variables are defined outside of click event*/
+    var apps = $('#apps').is(':checked');
+    var mobile = $('#mobile').is(':checked');
+    var large = $('#large').is(':checked');
+    var design = $('#design').is(':checked');
+    var curly = $('#curly').is(':checked');
 
-  /*chooseLang does not receive input if variables are defined outside of click event*/
-  var apps = $('#apps').is(':checked');
-  var mobile = $('#mobile').is(':checked');
-  var large = $('#large').is(':checked');
-  var design = $('#design').is(':checked');
-  var curly = $('#curly').is(':checked');
+    chooseLang(apps, mobile, large, design);
+  });
 
-
-  chooseLang(curly, mobile, design, apps);
-});
-
-//event.preventDefault();
 });
